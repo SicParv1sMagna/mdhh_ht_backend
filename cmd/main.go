bfb909f33a8397402862af5cb43833b49fcc8cc8
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-10-09 23:12:08 +0300

    Main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/SicParv1sMagna/mdhh_backend/internal/api"
+)
+
+func main() {
+	application, err := api.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	application.StartServer()
 }
