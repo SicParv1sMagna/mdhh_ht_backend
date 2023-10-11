cf6b370e3ef99142dcaf22dfab92782e9d0a3fde
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-10-11 23:38:10 +0300

    cors

--- internal/api/server.go
+++ internal/api/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/SicParv1sMagna/mdhh_backend/internal/delivery"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 )
@@ -13,14 +14,19 @@ func (a *Application) StartServer() {
 
 	store := sessions.NewCookieStore([]byte("SuperSecretKey"))
 
+	// Настройка CORS
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	router.Use(cors.New(config))
+
 	user := router.Group("/user")
 	{
-		//	localhost:8080/user/register
+		// localhost:8080/user/register
 		user.POST("/register", func(ctx *gin.Context) {
 			delivery.RegisterUser(a.repository, ctx)
 		})
 
-		//	localhost:8080/user/login
+		// localhost:8080/user/login
 		user.POST("/login", func(ctx *gin.Context) {
 			delivery.AuthUser(a.repository, store, ctx)
 		})
