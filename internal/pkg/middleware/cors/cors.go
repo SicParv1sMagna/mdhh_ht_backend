e4dcd6cb5efac05706b86f684a8c164b277018d1
Alexey <ax.chinaev@yandex.ru>
2023-10-15 04:47:19 +0300

    fix cors

--- internal/pkg/middleware/cors/cors.go
+++ internal/pkg/middleware/cors/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
